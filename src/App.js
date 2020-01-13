564b86bdc465f9d20bdd7c3172f7b3eaeb267d4c
Paul Biit <paul.mcnamara@biit.fi>
2020-01-13 12:40:39 +0200

    search now in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const apiWeatKey = {
+  key: "ecafc943dd32c2ba10f8bbf388da6aab",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+      </main>
 Noptimists rule.
     </div>
   );
