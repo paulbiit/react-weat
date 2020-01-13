2069385f3374d2ef005cc5cc6f6ae0013c8c4387
Paul Biit <paul.mcnamara@biit.fi>
2020-01-13 14:32:20 +0200

    Nice location

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ const apiWeatKey = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    console.log('Date is ' + d);
+    let months = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+      "Friday", "Saturday"];
+    
+    let wday = days[d.getDay()];
+    let wdate = d.getDate();
+
+    let wmonth = months[d.getMonth()];
+    let wyear = d.getFullYear();
+
+    // eslint-disable-next-line no-template-curly-in-string
+    return `${wday} ${wdate} ${wmonth} ${wyear}`
+}
+
   return (
     <div className="App">
       <main>
@@ -16,6 +34,14 @@ function App() {
             className="search-bar"
             placeholder="Search..."
             />
+        </div>
+        <div className="location-box">
+          <div className="location">Helsinki, Finland</div>
+          <div className="weather-date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temperature">15 C</div>
+          <div className="weatherDesc">Sunny</div>
         </div>
       </main>
     </div>
